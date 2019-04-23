8b00be63a87760f27cc0c8d4c9dc802b74404610
pokermanx <dimkanka@gmail.com>
2019-04-23 16:29:18 +0300

    Added styles for navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavBarComponent } from './ui/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NavBarComponent, children: [
+
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
