f37b86070030209766adec6875c40f20befb7a36
pokermanx <dimkanka@gmail.com>
2019-04-25 19:05:09 +0300

    Added simple inputs

--- src/app/budgets/add-budget/add-budget.component.ts
+++ src/app/budgets/add-budget/add-budget.component.ts
@@ -1,24 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { WalletPeriod } from 'src/app/shared/models/wallet.model';
 
 @Component({
     selector: 'app-add-budget',
     templateUrl: './add-budget.component.html',
     styleUrls: ['./add-budget.component.scss']
 })
-export class AddBudgetComponent {
+export class AddBudgetComponent implements OnInit {
 
     isLoaded = true;
 
     addBudgetForm: FormGroup;
 
+    periods = WalletPeriod;
+
     constructor() {}
+
+    ngOnInit() {
+        this.init();
+    }
 
     init() {
         this.addBudgetForm = new FormGroup({
             name: new FormControl(),
             value: new FormControl(),
-            period: new FormControl(),
+            period: new FormControl(3),
             categories: new FormControl()
         });
     }
